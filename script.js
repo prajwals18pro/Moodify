13f9e41f28774fb9993d031bda654cc8e14239df
prajwals18pro <roopashivu894@gmail.com>
2026-09-14 18:32:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -498,9 +498,11 @@ const songs = {
         ["Before You Go", "Lewis Capaldi"],
         ["Dandelions", "Ruth B."],
         ["A Thousand Years", "Christina Perri"]
-    ]
-}
-};
+       
+       
+       
+       }
+       };
 
 /* =========================
    FIND MUSIC
